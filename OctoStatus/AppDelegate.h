7319f815d99b520d9ec4756607b416a7a976a0cb
Alex Ling <hkalexling@gmail.com>
2016-12-01 19:26:49 +0800

    Initial Commit

---
+++ OctoStatus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OctoStatus
+//
+//  Created by Alex Ling on 1/12/2016.
+//  Copyright © 2016 Alex Ling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OctoStatus/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OctoStatus
+//
+//  Created by Alex Ling on 1/12/2016.
+//  Copyright © 2016 Alex Ling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
